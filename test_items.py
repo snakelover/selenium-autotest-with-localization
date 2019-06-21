69aae5a528baa2a7b168b9af96be030d51fd65d4
Savva Morozov <pythontheback@gmail.com>
2019-06-22 02:00:49 +0300

    Remove variable for storing 'buton' element

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_existence_on_localized_page(browser):
     browser.get(link)
     sleep(5)
-    button = browser.find_element_by_class_name("btn-add-to-basket")
+    browser.find_element_by_class_name("btn-add-to-basket")
