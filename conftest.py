7ac3338e6dcb8da3c8732690b2f23cd3105d8307
Savva Morozov <pythontheback@gmail.com>
2019-06-22 01:56:04 +0300

    Change some apostrophes

--- conftest.py
+++ conftest.py
@@ -6,12 +6,12 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru',
                      help="Specify language")
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def browser(request):
-    user_language = request.config.getoption("language")
+    user_language = request.config.getoption('language')
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
+    print("\nquit browser...")
     browser.quit()
